6eb5eb0e32c8d7a13e0cd38b757145c9b99c1e5c
Gonzalo <gcarvajalb@gmail.com>
2022-05-16 15:44:28 -0400

    2022

--- archivos_base_experiencia_VGA_2021/vivado_test_vga/nexys_vga_2020/nexys_vga_2020.ip_user_files/sim_scripts/clk_wiz_0/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../nexys_vga_2020.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
-  "../../../../nexys_vga_2020.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
