3e9f95fde44e056646e4ae5ed27b4262116d15a2
mauricio_siana <mauricio@siana-systems.com>
2022-07-05 01:13:01 -0400

    Se agregaron más comentarios y encabezados a los módulos UART

--- UART_reference_files/UART_ME_reference/UART_master.srcs/sources/uart/uart_basic.v
+++ UART_reference_files/UART_ME_reference/UART_master.srcs/sources/uart/uart_basic.v
@@ -5,23 +5,25 @@
  * @brief
  * Universal Asynchronous Receiver/Transmitter.
  * 
- * @param clk
- * @param reset
- * @param rx
- * @param rx_data
- * @param rx_ready
- * @param tx
- * @param tx_start
- * @param tx_data
- * @param tx_busy
+ * This mudule implements a full duplex UART. 
+ *
+ * @param clk         The FPGA clock
+ * @param reset       The reset
+ * @param rx          The reception signal
+ * @param rx_data     The reception data
+ * @param rx_ready    To advice when a new rx data is available
+ * @param tx          The tx signal
+ * @param tx_start    To start the rx data transmission
+ * @param tx_data     The data to transmit
+ * @param tx_busy     To advice the uart is or not sending data.
  */
 
 `timescale 1ns / 1ps
 
 module uart_basic
 #(
-	parameter CLK_FREQUENCY = 100000000,
-	parameter BAUD_RATE = 115200
+	parameter CLK_FREQUENCY = 100_000_000,
+	parameter BAUD_RATE = 115_200
 )(
 	input clk,
 	input reset,
@@ -40,6 +42,7 @@ module uart_basic
 	reg rx_ready_sync;
 	wire rx_ready_pre;
 
+  /*This instance is used to generate the RX tick clock with 8 oversampling*/
 	uart_baud_tick_gen #(
 		.CLK_FREQUENCY(CLK_FREQUENCY),
 		.BAUD_RATE(BAUD_RATE),
@@ -50,6 +53,7 @@ module uart_basic
 		.tick(baud8_tick)
 	);
 
+  /*This instance implements the uart reception*/
 	uart_rx uart_rx_blk (
 		.clk(clk),
 		.reset(reset),
@@ -59,22 +63,24 @@ module uart_basic
 		.rx_ready(rx_ready_pre)
 	);
 
+  /*To generate a pulse when the rx_ready_pre is triggered*/
 	always @(posedge clk) begin
 		rx_ready_sync <= rx_ready_pre;
 		rx_ready <= ~rx_ready_sync & rx_ready_pre;
 	end
 
-
+  /*This instance is used  to generate the TX tick clock with 1 oversampling */
 	uart_baud_tick_gen #(
 		.CLK_FREQUENCY(CLK_FREQUENCY),
 		.BAUD_RATE(BAUD_RATE),
 		.OVERSAMPLING(1)
 	) baud_tick_blk (
 		.clk(clk),
-		.enable(tx_busy),
+		.enable(tx_busy), /*Enabled only when it is necessary to send data*/
 		.tick(baud_tick)
 	);
 
+  /*This instance implements the uart transmission*/
 	uart_tx uart_tx_blk (
 		.clk(clk),
 		.reset(reset),
--- UART_reference_files/UART_ME_reference/UART_master.srcs/sources/uart/uart_baud_tick_gen.v
+++ UART_reference_files/UART_ME_reference/UART_master.srcs/sources/uart/uart_baud_tick_gen.v
@@ -1,17 +1,23 @@
 /*
  * uart_baud_tick_gen.v
  * 2017/02/01 - Felipe Veas <felipe.veasv at usm.cl>
+ * 2022/07/04 - Mauricio Solis: Add some comments abd headers.
  *
+ * @brief 
  * Baud clock generator for UART, original code from:
  * http://fpga4fun.com/SerialInterface.html
+ *
+ * @param clk:      The FPGA clock.
+ * @param enable:   To enable/disable the tick generator.
+ * @param tick:     The tick base time output.
  */
 
 `timescale 1ns / 1ps
 
 module uart_baud_tick_gen
 #(
-	parameter CLK_FREQUENCY = 25000000,
-	parameter BAUD_RATE = 115200,
+	parameter CLK_FREQUENCY = 25_000_000,
+	parameter BAUD_RATE = 115_200,
 	parameter OVERSAMPLING = 1
 )(
 	input clk,
@@ -19,6 +25,7 @@ module uart_baud_tick_gen
 	output tick
 );
 
+  /*Base 2 Logarithm function*/
 	function integer clog2;
 		input integer value;
 		begin
@@ -28,11 +35,11 @@ module uart_baud_tick_gen
 		end
 	endfunction
 
-	localparam ACC_WIDTH = clog2(CLK_FREQUENCY / BAUD_RATE) + 8;
-	localparam SHIFT_LIMITER = clog2(BAUD_RATE * OVERSAMPLING >> (31 - ACC_WIDTH));
+	localparam ACC_WIDTH = clog2(CLK_FREQUENCY / BAUD_RATE) + 8;                      /* The Accumulator width*/
+	localparam SHIFT_LIMITER = clog2(BAUD_RATE * OVERSAMPLING >> (31 - ACC_WIDTH));   /* The Shift limiter to prevent overflow in calculus*/
 	localparam INCREMENT =
 			((BAUD_RATE * OVERSAMPLING << (ACC_WIDTH - SHIFT_LIMITER)) +
-			(CLK_FREQUENCY >> (SHIFT_LIMITER + 1))) / (CLK_FREQUENCY >> SHIFT_LIMITER);
+			(CLK_FREQUENCY >> (SHIFT_LIMITER + 1))) / (CLK_FREQUENCY >> SHIFT_LIMITER);   /* The accumulator increment*/
 
 	(* keep = "true" *)
 	reg [ACC_WIDTH:0] acc = 0;
