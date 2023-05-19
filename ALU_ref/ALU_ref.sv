81edb3be87e60c579a5996782d43828338745846
TomasVelasquez <tomas.velasquez@sansano.usm.cl>
2023-05-19 13:42:51 -0400

    se reemplaza la alu_ref por la nueva version 2023

--- ALU_ref/ALU_ref.sv
+++ ALU_ref/ALU_ref.sv
@@ -1,53 +1,49 @@
-// ALU de referencia.  Descrita por Oscar Rojas.
+`timescale 1ns / 1ps
 
-module ALU_ref #(parameter C_WIDTH = 8)
+module ALU_ref #(parameter M = 8)
 (
-	input logic [C_WIDTH-1:0] A,
-	input logic [C_WIDTH-1:0] B,
+	input logic [M-1:0] A,
+	input logic [M-1:0] B,
 	input logic [1:0] OpCode,
 
-	output logic [C_WIDTH-1:0] Result,
-	output logic [3:0] Status
+	output logic [M-1:0] Result,
+	output logic [4:0] Flags
 );
-	logic N, Z, C, V; // ALU Flags
+	logic V, C, Z, N, P;
 
 	always_comb begin
 		case(OpCode)
-			2'd0: begin
-			    /* El operador {} se usa para concatenar bits que pueden venir de distintos buses.
-			       Notar que se esta guardando el resultado de la suma en un elemento que tiene un bit mas de ancho que los operandos,
-			       lo cual forzara a la herramienta a que extienda en un bit el ancho de los operandos, como si fuera un sumador de 9 bits.
-			       Es importante notar que el bit extra se usa para el carry flag y no es parte del resultado de la operacion. 
-			       Hay varias formas de describir esta operacion. Con practica iran aprendiendo trucos.
-			    */ 
-				{C, Result} = A + B; 
-				V = (Result[C_WIDTH-1] & ~A[C_WIDTH-1] & ~B[C_WIDTH-1]) | (~Result[C_WIDTH-1] & A[C_WIDTH-1] & B[C_WIDTH-1]);
-			    // Hay overflow si el resultado es positivo y los operandos son negativos, o si el resultado es positivo y los operandos son negativos
-			end
-
-			2'd1: begin
-				{C, Result} = A - B;
-				V = (Result[C_WIDTH-1] & ~A[C_WIDTH-1] & B[C_WIDTH-1]) | (~Result[C_WIDTH-1] & A[C_WIDTH-1] & ~B[C_WIDTH-1]);
-			   // Hay overflow si A es negativo, B es positivo, y Result es positivo; o bien si A es positivo, B es negativo, y Result es negativo.
-			end
-
-			2'd2: begin
-				Result = A | B;
+			2'b00: begin
+			    //NOR 
+				Result = ~(A | B);
 				C = 1'b0;
 				V = 1'b0;
 			end
 
-			2'd3: begin
-				Result = A & B;
+			2'b01: begin
+			    //NAND
+				Result = ~(A & B);
 				C = 1'b0;
 				V = 1'b0;
+			end
+
+			2'b10: begin
+			    //SUMA
+			    {C, Result} = A + B;
+				V = (Result[M-1] & ~A[M-1] & ~B[M-1]) | (~Result[M-1] & A[M-1] & B[M-1]);		
+			end
+
+			2'b11: begin
+			    //RESTA
+			    {C, Result} = A - B;
+				V = (Result[M-1] & ~A[M-1] & B[M-1]) | (~Result[M-1] & A[M-1] & ~B[M-1]);
 			end
 		endcase
 
-		N = Result[C_WIDTH-1]; // el flag N cablea directo al MSB de Result
-		Z = (Result == '0);    // revisa si el resultado es 0
+		N = Result[M-1];
+		Z = (Result == '0);
+		P = ~^Result;
 
-		// se usa la concatenacion para agrupar bits independiente en un bus.
-		Status = {N, Z, C, V};
+		Flags = {V, C, Z, N, P};
 	end
 endmodule
