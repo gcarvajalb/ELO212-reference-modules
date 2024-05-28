13beb1dfe84df870d22a30e4112a696929d6c907
gcarvajalb <gcarvajalb@gmail.com>
2024-05-28 16:38:08 -0400

    Update ALU_ref.sv

--- ALU_ref/ALU_ref.sv
+++ ALU_ref/ALU_ref.sv
@@ -14,36 +14,37 @@ module ALU_ref #(parameter M = 8)
 	always_comb begin
 		case(OpCode)
 			2'b00: begin
-			    //NOR 
-				Result = ~(A | B);
-				C = 1'b0;
-				V = 1'b0;
+			    //RESTA
+			    {C, Result} = A - B;
+				V = (Result[M-1] & ~A[M-1] & B[M-1]) | (~Result[M-1] & A[M-1] & ~B[M-1]);
 			end
 
 			2'b01: begin
-			    //NAND
-				Result = ~(A & B);
-				C = 1'b0;
-				V = 1'b0;
-			end
-
-			2'b10: begin
 			    //SUMA
 			    {C, Result} = A + B;
 				V = (Result[M-1] & ~A[M-1] & ~B[M-1]) | (~Result[M-1] & A[M-1] & B[M-1]);		
 			end
 
-			2'b11: begin
-			    //RESTA
-			    {C, Result} = A - B;
-				V = (Result[M-1] & ~A[M-1] & B[M-1]) | (~Result[M-1] & A[M-1] & ~B[M-1]);
+			2'b10: begin
+			    //OR 
+				Result = (A | B);
+				C = 1'b0;
+				V = 1'b0;
 			end
+
+			2'b11: begin
+			    //AND
+				Result = (A & B);
+				C = 1'b0;
+				V = 1'b0;
+			end
+
 		endcase
 
 		N = Result[M-1];
 		Z = (Result == '0);
-		P = ~^Result;
+		P = ^Result;
 
-		Flags = {V, C, Z, N, P};
+		Flags = {N, Z, C, V, P};
 	end
 endmodule
